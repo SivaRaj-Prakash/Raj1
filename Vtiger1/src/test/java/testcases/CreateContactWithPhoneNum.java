ab6bf29e2f9492aef4122b129d1c7066a84e3b60
Prakashraj024 <m.prakashraj996@gmail.com>
2025-07-22 09:28:48 +0530

    2 changes done

--- Vtiger1/src/test/java/testcases/CreateContactWithPhoneNum.java
+++
@@ -1,9 +0,0 @@
-package testcases;
-
-public class CreateContactWithPhoneNum {
-
-	public static void main(String[] args) {
-    System.out.println("Contact is created with Phone Num");
-	}
-
-}
---
+++ Vtiger1/src/test/java/testcases/CreateOrgWithProduct.java
@@ -0,0 +1,9 @@
+package testcases;
+
+public class CreateOrgWithProduct {
+
+	public static void main(String[] args) {
+    System.out.println("Org with Product Details is created");
+	}
+
+}
